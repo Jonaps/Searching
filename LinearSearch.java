b2bd5c3d8f0d61f9f7ce0a277a6ace2e515f3d1d
Jonaps <105373463+Jonaps@users.noreply.github.com>
2022-06-02 09:33:30 -0400

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,17 @@
+
+
+public class LinearSearch {
+    public static boolean linearSearch(int[] arr, int key) {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == key) {
+                return true;
+            }       
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
